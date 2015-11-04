22b82adc4b81dd782421dc1fd9ea324677002ba2
Stephen Larson <stephen.larson@gmail.com>
2015-11-04 15:43:14 -0800

    adding first stub version of master script

---
+++ master_openworm.py
@@ -0,0 +1,28 @@
+print("OpenWorm Master Script v.0.1")
+print("")
+print("This script attempts to run a full pass through the OpenWorm scientific libraries.")
+print("This depends on several other repositories being loaded to work")
+print("It will report out where steps are missing."
+print("Eventually all the steps will be filled in.")
+print("")
+
+print("Step 1: Rebuild c302 from the latest PyOpenWorm")
+print("not yet implemented.")
+
+print("Step 2: Execute the latest c302 simulation")
+print("not yet implemented.")
+
+print("Step 3: Feed muscle activations into Sibernetic")
+print("not yet implemented.")
+
+print("Step 4: Run Sibernetic")
+print("not yet implemented.")
+
+print("Step 5: Extract skeleton from Sibernetic run for movement analysis")
+print("not yet implemented.")
+
+print("Step 6: Run movement analysis")
+print("not yet implemented.")
+
+print("Step 7: Report on movement analysis fit to real worm videos")
+print("not yet implemented.")
