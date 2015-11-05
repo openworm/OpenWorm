e089475a49bf51182bdd8d1e7ec5cd9493747719
Stephen Larson <stephen.larson@gmail.com>
2015-11-04 19:42:21 -0800

    new edits to incorporate new steps into the process
    most of this doesn’t work yet but starts to sketch out the steps

--- master_openworm.py
+++ master_openworm.py
@@ -8,10 +8,14 @@ print("It will report out where steps are missing.")
 print("Eventually all the steps will be filled in.")
 print("")
 
+print("****************************")
 print("Step 1: Rebuild c302 from the latest PyOpenWorm")
+print("****************************")
 print("not yet implemented.")
 
+print("****************************")
 print("Step 2: Execute the latest c302 simulation")
+print("****************************")
 from subprocess import call
 import sys, os
 try:
@@ -22,27 +26,48 @@ except:
     print "Unexpected error:", sys.exc_info()[0]
     raise
 
+print("****************************")
 print("Step 3: Feed muscle activations into Sibernetic")
-print("not yet implemented.")
+print("****************************")
+try:
+    os.chdir("/sibernetic/src")
+    call(["wget", "https://github.com/pgleeson/Smoothed-Particle-Hydrodynamics/blob/master/src/main_sim.py"])
+    call(["python", "main_sim.py"])
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
 
+print("****************************")
 print("Step 4: Run Sibernetic")
+print("****************************")
 try:
     os.chdir("/sibernetic")
     call(["apt-get", "install", "make"])
     call(["make", "clean"])
     call(["make", "all"])
-    call(["Xvfb", ":1", "-screen", "0", "1024x768x16 &>xvfb.log &"])
+    call(["Xvfb", ":1", "-screen", "0", "1024x768x16"])
     call(["export", "DISPLAY=:1.0"])
     call(["./Release/Sibernetic"])
 except:
     print "Unexpected error:", sys.exc_info()[0]
     raise
 
+print("****************************")
 print("Step 5: Extract skeleton from Sibernetic run for movement analysis")
+print("****************************")
 print("not yet implemented.")
 
+print("****************************")
 print("Step 6: Run movement analysis")
-print("not yet implemented.")
+print("****************************")
+try:
+    os.chdir("/movement_validation/tests")
+    call(["nosetests", "--nocapture"])
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
 
+print("****************************")
 print("Step 7: Report on movement analysis fit to real worm videos")
+print("****************************")
 print("not yet implemented.")
