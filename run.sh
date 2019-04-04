990365d9f1e877405b1a5a1eace127817f8f71b5
Mark Watts <watts.mark2015@gmail.com>
2019-04-04 18:03:20 -0500

    Using PyOpenWorm dev branch
    - Also, upgrading pip (avoids some issues with using wheel packages)

--- run.sh
+++ run.sh
@@ -19,28 +19,21 @@ xhost +
 
 if [ -z "$duration" ]
 then #duration is not set, don't use it
-  docker run -d \
-    --name openworm \
-    --device=/dev/dri:/dev/dri \
-    -e DISPLAY=$DISPLAY \
-    -e OW_OUT_DIR=$OW_OUT_DIR \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    --privileged \
-    -v $HOST_OUT_DIR:$OW_OUT_DIR:rw \
-    openworm/openworm:0.9 \
-    bash -c "DISPLAY=:44 python master_openworm.py"
+    DURATION_PART=""
 else #Duration is set, use it.
-  docker run -d \
-    --name openworm \
-    --device=/dev/dri:/dev/dri \
-    -e DISPLAY=$DISPLAY \
-    -e DURATION=$duration \
-    -e OW_OUT_DIR=$OW_OUT_DIR \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    --privileged \
-    -v $HOST_OUT_DIR:$OW_OUT_DIR:rw \
-    openworm/openworm:0.9 \
-    bash -c "DISPLAY=:44 python master_openworm.py"
+    DURATION_PART="-e DURATION=$duration"
 fi
+
+docker run -d \
+--name openworm \
+--device=/dev/dri:/dev/dri \
+-e DISPLAY=$DISPLAY \
+$DURATION_PART \
+-e OW_OUT_DIR=$OW_OUT_DIR \
+-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+--privileged \
+-v $HOST_OUT_DIR:$OW_OUT_DIR:rw \
+openworm/openworm:0.9 \
+bash -c "DISPLAY=:44 python master_openworm.py"
 
 docker logs -f openworm
