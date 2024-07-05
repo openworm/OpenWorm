e29a7753988e116fc7567d7a6d4af4a1e68a009e
Tijs Zwinkels <tijs@tinkertank.eu>
2024-07-05 11:54:04 +0200

    auto-detect arm platform in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-version=$(<VERSION) # Read version of Dockerfile from file VERSION
-docker build "$@" -t "openworm/openworm:$version" .
+version=$(<VERSION)
+
+# Set the platform flag if we're on ARM
+arch=$(uname -m)
+if [[ "$arch" == "arm64" || "$arch" == "aarch64" ]]; then
+    platform_flag="--platform linux/amd64"
+else
+    platform_flag=""
+fi
+
+# Build the Docker image
+docker build $platform_flag "$@" -t "openworm/openworm:$version" .
