2b1988120579bca963a372c3eacc15bea504831e
Padraig Gleeson <p.gleeson@gmail.com>
2025-08-08 14:44:22 +0100

    Improve options o run.sh, able to pass a configuration, e.g. ./run.sh -d 20 -c demo1

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+version=$(<VERSION) # Read version of Dockerfile from file VERSION
+
+
 #from: https://unix.stackexchange.com/a/129401
-while getopts ":d:p:" opt; do
-  case $opt in
+while getopts ":d:c:" opt; do
+  case "${opt}" in
     d) duration="$OPTARG"
     ;;
-    p) p_out="$OPTARG"
+    c) configuration="$OPTARG"
     ;;
-    \?) echo "Invalid option -$OPTARG" >&2
+    *) echo "Usage: $0 [-d duration] [-c configuration]" >&2
+       exit 1
     ;;
   esac
 done
@@ -15,7 +19,6 @@ done
 OW_OUT_DIR=/home/ow/shared
 HOST_OUT_DIR=$PWD
 
-version=$(<VERSION) # Read version of Dockerfile from file VERSION
 
 xhost +
 
@@ -26,10 +29,24 @@ else #Duration is set, use it.
     DURATION_PART="-e DURATION=$duration"
 fi
 
+echo 888
+if [ -z "$configuration" ]
+then #configuration is not set, don't use it
+    CONFIGURATION_PART=""
+else #Configuration is set, use it.
+    CONFIGURATION_PART="-e CONFIGURATION=$configuration"
+fi
+
+echo 77
+
+echo "Running Docker container for OpenWorm v${version}, configuration: ${configuration}, duration: ${duration}"
+
+
 docker run -d \
 --name openworm_$version \
 --device=/dev/dri:/dev/dri \
 -e DISPLAY=$DISPLAY \
+$CONFIGURATION_PART \
 $DURATION_PART \
 -e OW_OUT_DIR=$OW_OUT_DIR \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
