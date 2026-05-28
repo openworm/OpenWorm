e36c031f23e36b2df84c83479f5d2efa1be7429e
Padraig Gleeson <p.gleeson@gmail.com>
2026-05-28 09:51:36 +0100

    Run more tests

---
+++ run-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OW_OUT_DIR=/home/ow/shared
+HOST_OUT_DIR=$PWD
+
+version=$(<VERSION) # Read version of Dockerfile from file VERSION
+
+xhost +
+
+docker run -ti \
+  --name openworm_$version \
+  --device=/dev/dri:/dev/dri \
+  -e DISPLAY=$DISPLAY \
+  -e OW_OUT_DIR=$OW_OUT_DIR \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --privileged \
+  -v $HOST_OUT_DIR:$OW_OUT_DIR:rw \
+  openworm/openworm:$version \
+  bash -c "cd /home/ow/sibernetic && time ./test.sh"
