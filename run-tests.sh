facca94b0d8a61805fc5c8c7190259e080a6263a
Padraig Gleeson <p.gleeson@gmail.com>
2026-05-28 10:10:59 +0100

    remove xhosts issue

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@ HOST_OUT_DIR=$PWD
 
 version=$(<VERSION) # Read version of Dockerfile from file VERSION
 
-xhost +
+xhost + || true # allow connections to X server, don't throw an error if it fails
 
 docker run -ti \
   --name openworm_$version \
